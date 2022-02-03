4e63616ebfde6bf485d6464426714a631aea7d8a
Ruwani <ruwanide51@gmail.com>
2022-02-03 16:00:04 -0500

    feat: set up repo

---
+++ apps/api/src/app/app.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { Message } from '@tempus/api-interfaces';
+
+@Injectable()
+export class AppService {
+  getData(): Message {
+    return { message: 'Welcome to api!' };
+  }
+}
