95e2c7edf648d60160ec268de291fed1cb34f994
Mustafa Ali <alimustafa0987@gmail.com>
2022-02-05 12:01:40 -0500

    feat: add core module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { CoreModule } from '@tempus/core'
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), CoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
