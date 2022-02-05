f848737b89eee21000ec48a71dac58ec6606c770
Mustafa Ali <alimustafa0987@gmail.com>
2022-02-05 12:08:30 -0500

    feat: config service use in app module

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,14 +7,17 @@ import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './app/app.module'
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const config = app.get(ConfigService)
   const globalPrefix = 'api'
   app.setGlobalPrefix(globalPrefix)
   const port = process.env.PORT || 3333
   await app.listen(port)
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`)
+  Logger.log(`Running in ${config.get('environment')} mode`)
 }
 
 bootstrap()
