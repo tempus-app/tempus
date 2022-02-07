dcea918d756dc3b5680acd63fec3dd221576d9e9
Ruwani <ruwanide51@gmail.com>
2022-02-06 22:58:18 -0500

    chore(db): set up postgres container

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # tempus
+
+### Set up
+
+1. Run ``npm i``
+2. Set Up a .env file: 
+```NODE_ENV=development
+PORT=3000
+```
+2. Run ``npm run start:api``
+
--- ormconfig.ts
+++ ormconfig.ts
@@ -1,11 +1,15 @@
-import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions'
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions'
 import { User, Resource } from '@tempus/account'
 import { View, Education, Skill, Experience, Revision, SkillType } from '@tempus/profile'
 import { Client, Project, Task } from '@tempus/project'
 
-const config: SqliteConnectionOptions = {
-  type: 'sqlite',
-  database: './db',
+const config: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [User, Resource, View, Education, Skill, Experience, Revision, SkillType, Client, Project, Task],
   synchronize: true,
 }
