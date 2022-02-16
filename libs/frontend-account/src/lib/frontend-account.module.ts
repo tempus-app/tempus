7be22180b979e08b19c3fe7bd6db5f32e0856ae0
Afrah Ali <aali179@uottawa.ca>
2022-02-16 18:53:52 -0500

    chore(frontend): use module naming conventions

--- libs/frontend-account/src/lib/frontend-account.module.ts
+++ libs/frontend-account/src/lib/frontend-account.module.ts
@@ -4,4 +4,4 @@ import { CommonModule } from '@angular/common'
 @NgModule({
   imports: [CommonModule],
 })
-export class FrontendAccountModule {}
+export class AccountModule {}
--- libs/frontend-common/src/lib/frontend-common.module.ts
+++ libs/frontend-common/src/lib/frontend-common.module.ts
@@ -4,4 +4,4 @@ import { CommonModule } from '@angular/common'
 @NgModule({
   imports: [CommonModule],
 })
-export class FrontendCommonModule {}
+export class CommonModule {}
