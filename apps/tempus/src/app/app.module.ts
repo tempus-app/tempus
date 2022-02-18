6ddf3eafdd71604f8205ec04f877d70dbdadef30
Afrah Ali <43392705+aali179@users.noreply.github.com>
2022-02-18 17:04:09 -0500

    feat(frontend): setup-theming (#34)
    * feat(frontend): adding theme
    
    * feat(frontend): add alert theme
    
    * chore(frontend): add angular dependencies
    
    * chore(frontend): replace @import with @use
    
    * chore(frontend): shorten path

--- apps/tempus/src/app/app.module.ts
+++ apps/tempus/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { MatInputModule } from '@angular/material/input'
+import { MatButtonModule } from '@angular/material/button'
 
 import { AppComponent } from './app.component'
 import { NxWelcomeComponent } from './nx-welcome.component'
@@ -7,7 +9,7 @@ import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, MatInputModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
