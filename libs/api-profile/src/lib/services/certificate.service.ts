aa834ac62da049fa3f655f11eaa75e6b646a17d3
gcham5 <gcham042@uottawa.ca>
2022-02-18 16:11:03 -0500

    feat(api): add certification service

---
+++ libs/api-profile/src/lib/services/certificate.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, NotImplementedException } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { Certification } from '../models/certification.model'
+import { CertificationEntity } from '../entities/certification.entity'
+
+@Injectable()
+export class CertificationService {
+  constructor(
+    @InjectRepository(CertificationEntity)
+    private certificationRepository: Repository<CertificationEntity>,
+  ) {}
+
+  // create ceritifcation for a specific resource
+  createCertification(resourceId: number, certification: Omit<Certification, 'id'>): Promise<CertificationEntity> {
+    throw new NotImplementedException()
+  }
+
+  // return all certifications by resource
+  findCertificationByResource(resourceId: number): Promise<Certification> {
+    throw new NotImplementedException()
+  }
+
+  // return certification by id
+  findCertificationById(certificationId: number): Promise<Certification> {
+    throw new NotImplementedException()
+  }
+
+  // edit certification
+  editCertification(certification: Certification): Promise<Certification> {
+    throw new NotImplementedException()
+  }
+
+  // delete certification
+  deleteCertification(certificationId: number) {
+    throw new NotImplementedException()
+  }
+}
