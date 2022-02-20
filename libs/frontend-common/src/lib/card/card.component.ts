75a9333622c6a97efd014cc2ff40401697125443
Ruwani <ruwanide51@gmail.com>
2022-02-20 16:49:51 -0500

    feat(frontend): create card wrapper

---
+++ libs/frontend-common/src/lib/card/card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tempus-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
