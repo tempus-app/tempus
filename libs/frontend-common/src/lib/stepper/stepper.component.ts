4ac389ae0be076c858c48095ee090599674fc7a6
aali179 <aali179@uottawa.ca>
2022-02-22 18:33:06 -0500

    feat(frontend): supply color palette

--- libs/frontend-common/src/lib/stepper/stepper.component.ts
+++ libs/frontend-common/src/lib/stepper/stepper.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core'
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper'
 import { MatIconModule } from '@angular/material/icon'
 import { MatStep } from '@angular/material/stepper'
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'tempus-stepper',
@@ -17,7 +17,8 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
   encapsulation: ViewEncapsulation.None,
 })
 export class StepperComponent implements OnInit {
-  @Input() steps: Array<String> = [];
+  @Input() steps: Array<String> = []
+  @Input() color: string = 'primary'
   constructor() {}
 
   ngOnInit(): void {}
