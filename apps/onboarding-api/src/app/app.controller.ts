71141ed953af8fc9b5e68b3e86dd892542d9c688
= <gabriel.granata@hotmail.com>
2022-02-21 20:11:16 -0500

    Swagger OpenAPI documentation support

--- apps/onboarding-api/src/app/app.controller.ts
+++ apps/onboarding-api/src/app/app.controller.ts
@@ -1,18 +1,25 @@
-import { Controller, Get } from '@nestjs/common'
-
+import { Body, Controller, Get, Post } from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
 import { Message } from '@tempus/api-interfaces'
+import { SlimUserDto } from '@tempus/datalayer'
 
 import { AppService } from './app.service'
 
+@ApiTags('app')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Post('create-user')
+  async createUserSwaggerExample(@Body() createUserDto: SlimUserDto) {
+    this.appService.createUser();
+  }
 
   @Get('hello')
   getData(): Message {
     return this.appService.getData()
   }
-
+  
   @Get('create')
   makeUser() {
     this.appService.createUser()
