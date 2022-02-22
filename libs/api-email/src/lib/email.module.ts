51bf62598f49ffd8e343f4b8021d97d733b3750c
Ruwani <ruwanide51@gmail.com>
2022-02-22 18:08:44 -0500

    feat(api): email service

---
+++ libs/api-email/src/lib/email.module.ts
@@ -0,0 +1,39 @@
+import { MailerModule } from '@nestjs-modules/mailer'
+import { Module  } from '@nestjs/common'
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter'
+
+import { ConfigService } from '@nestjs/config'
+import { EmailService } from '.'
+import { DataLayerModule } from '@tempus/datalayer'
+
+@Module({
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: async (config: ConfigService) => ({
+        transport: {
+          host: config.get('EMAIL_HOST'),
+          port: config.get('EMAIL_PORT'),
+          secure: false,
+          requireTLS: true,
+          auth: {
+            user: config.get('EMAIL_USERNAME'),
+            pass: config.get('EMAIL_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: `"No Reply" <${config.get('EMAIL_ADDRESS')}>`,
+        },
+        template: {
+          dir: __dirname + '/templates',
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+        inject: [ConfigService],
+      }),
+    }),
+  ],
+  providers: [EmailService]
+})
+export class EmailModule {}
---
+++ libs/api-email/src/lib/services/email.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common'
+import { MailerService } from '@nestjs-modules/mailer'
+import { LinkEntity } from '@tempus/datalayer'
+
+@Injectable()
+export class EmailService {
+  constructor(private readonly mailerService: MailerService) {}
+
+  async sendInvitationEmail(link: LinkEntity): Promise<void> {
+    await this.mailerService.sendMail({
+      to: link.email, // list of receivers
+      subject: 'Complete your Application for CAL & Associates', // Subject line
+      template: 'invitationLink',
+      context: {
+        code: 'cf1a3f828287',
+        name: `${link.firstName} ${link.lastName}`,
+      },
+    })
+  }
+}
