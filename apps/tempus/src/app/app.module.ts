324a588b311a94eefc63159e0e66859dd7419c11
Aman <amanriat777@gmail.com>
2022-02-22 17:08:22 -0500

    feat(frontend): "add initial sign in card"

--- apps/tempus/src/app/app.module.ts
+++ apps/tempus/src/app/app.module.ts
@@ -6,12 +6,18 @@ import { MatButtonModule } from '@angular/material/button'
 import { AppComponent } from './app.component'
 import { NxWelcomeComponent } from './nx-welcome.component'
 import { HttpClientModule } from '@angular/common/http'
-import { FrontendCommon } from '@tempus/frontend-common'
+import { FrontendCommon } from '@tempus/frontend-common';
+import { SignInComponent } from './sign-in/sign-in.component'
+import {MatGridListModule} from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card'
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule, MatInputModule, MatButtonModule, FrontendCommon],
+  declarations: [AppComponent, NxWelcomeComponent, SignInComponent],
+  imports: [BrowserModule, HttpClientModule, MatInputModule, MatButtonModule, FrontendCommon, MatGridListModule, MatCardModule],
   providers: [],
   bootstrap: [AppComponent],
+  exports: [
+    SignInComponent
+  ],
 })
 export class AppModule {}
