4226c9dbf9890d60194acb86dce7b6eee43b25de
Aman <amanriat777@gmail.com>
2022-02-22 21:24:56 -0500

    feat(frontend): "add template sign in page"

--- apps/tempus/src/app/app.module.ts
+++ apps/tempus/src/app/app.module.ts
@@ -7,17 +7,14 @@ import { AppComponent } from './app.component'
 import { NxWelcomeComponent } from './nx-welcome.component'
 import { HttpClientModule } from '@angular/common/http'
 import { FrontendCommon } from '@tempus/frontend-common';
-import { SignInComponent } from './sign-in/sign-in.component'
+import { FrontendSignUpModule } from '@tempus/frontend-sign-up';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card'
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, SignInComponent],
-  imports: [BrowserModule, HttpClientModule, MatInputModule, MatButtonModule, FrontendCommon, MatGridListModule, MatCardModule],
+  declarations: [AppComponent, NxWelcomeComponent],
+  imports: [BrowserModule, HttpClientModule, MatInputModule, MatButtonModule, FrontendCommon, MatGridListModule, MatCardModule, FrontendSignUpModule],
   providers: [],
   bootstrap: [AppComponent],
-  exports: [
-    SignInComponent
-  ],
 })
 export class AppModule {}
