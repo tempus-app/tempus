101f8cfc7477319c81f1e31f9015213c6867a845
Afrah Ali <43392705+aali179@users.noreply.github.com>
2022-02-24 20:55:06 -0500

    chore(frontend): support lib imports (#85)
    * feat(frontent): make frontend-common buildable
    
    * feat(frontend): import theme
    
    * fix(frontend): update theme import

--- apps/tempus/src/app/app.module.ts
+++ apps/tempus/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { HttpClientModule } from '@angular/common/http'
 import { FrontendCommon } from '@tempus/frontend-common'
 import { MatTooltipModule } from '@angular/material/tooltip'
 import { MatPaginatorModule } from '@angular/material/paginator'
-import {FrontendSignUpModule} from '@tempus/frontend-sign-up'
+import { FrontendSignUpModule } from '@tempus/frontend-sign-up'
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
@@ -21,7 +21,7 @@ import {FrontendSignUpModule} from '@tempus/frontend-sign-up'
     FrontendCommon,
     MatTooltipModule,
     MatPaginatorModule,
-    FrontendSignUpModule
+    FrontendSignUpModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
