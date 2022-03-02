3972d86f61bfabc526f34e6b59e7b3682d003b3c
Aman Riat <amanriat777@gmail.com>
2022-03-02 18:48:58 -0500

    feat(frontend): add skeleton my info page 1 and-2 (#100)
    * feat(frontend): add initial component
    
    * feat(frontend):"add skeleton my-info-1-2 pages"
    
    * feat(frontend): modify styling
    
    * feat(frontend): add styling to theme.scss sheet
    
    * feat(frontend): add missing packages
    
    * feat(frontend): address PR comments
    
    * feat(frontend): handle work experience edge case
    
    * feat(frontend): add autocomplete for country
    
    * feat(api): accept enums into text input
    
    * chore: remove my info 2 page from home
    
    Co-authored-by: Mustafa Ali <alimustafa0987@gmail.com>

---
+++ libs/frontend-common/src/lib/text-input/text-input-enum.ts
@@ -0,0 +1,13 @@
+export enum InputType {
+	BUTTON = 'button',
+	CHECKBOX = 'checkbox',
+	DATE = 'date',
+	EMAIL = 'email',
+	FILE = 'file',
+	NUMBER = 'number',
+	PASSWORD = 'password',
+	RADIO = 'radio',
+	SUBMIT = 'submit',
+	TEXT = 'text',
+	TIME = 'time',
+}
--- libs/frontend-common/src/lib/text-input/text-input.component.ts
+++ libs/frontend-common/src/lib/text-input/text-input.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { InputType } from './text-input-enum';
 
 @Component({
 	selector: 'tempus-text-input',
 	templateUrl: './text-input.component.html',
 	styleUrls: ['./text-input.component.scss'],
 })
-export class TextInputComponent implements OnInit {
+export class TextInputComponent {
 	@Input() placeholder = '';
 
 	@Input() cssClass = '';
 
-	constructor() {}
+	@Input() id = '';
 
-	ngOnInit(): void {}
+	@Input() inputType: InputType = InputType.TEXT;
 }
