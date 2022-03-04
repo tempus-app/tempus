8c847ca4730dfd350b4c5b8cde31714c4844d37a
Ruwani <ruwanide51@gmail.com>
2022-03-04 17:13:31 -0500

    chore: accomodate file struct change

--- libs/client/onboarding-client/signup/features/feature-upload-resume/src/lib/uploadresume/uploadresume.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'tempus-upload-resume',
-	templateUrl: './uploadresume.component.html',
-	styleUrls: ['./uploadresume.component.scss'],
-})
-export class UploadResumeComponent {}
