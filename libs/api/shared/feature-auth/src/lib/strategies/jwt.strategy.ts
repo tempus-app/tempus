8d0e5571287474a20bbd95680d47908ce8c7eea3
gcham5 <gcham042@uottawa.ca>
2022-03-05 00:12:41 -0500

    chore(api): rename jwt_secret

--- libs/api/shared/feature-auth/src/lib/strategies/jwt.strategy.ts
+++ libs/api/shared/feature-auth/src/lib/strategies/jwt.strategy.ts
@@ -10,7 +10,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 			ignoreExpiration: false,
-			secretOrKey: configService.get('JWT_SECRET'),
+			secretOrKey: configService.get('jwtSecret'),
 		});
 	}
 
