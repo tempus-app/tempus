818bcb862751ee6ce41e29f573af27e549e7fff8
ruwani ☁️ <50240905+ruwanidealwis@users.noreply.github.com>
2022-03-09 17:57:19 -0500

    feat(frontend): landing page + sign up page (#134)
    * feat: create sign up component
    
    * feat: modify text input
    
    * feat: add landing component
    
    * feat: add pseudo routing logic
    
    * chore: update package lock
    
    * feat: add consent checkbox

--- libs/client/onboarding-client/signup/features/feature-create-credentials/src/lib/credentials/credentials.component.ts
+++ libs/client/onboarding-client/signup/features/feature-create-credentials/src/lib/credentials/credentials.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Link } from '@tempus/shared-domain';
 
 @Component({
 	selector: 'tempus-credentials',
 	templateUrl: './credentials.component.html',
 	styleUrls: ['./credentials.component.scss'],
 })
-export class CredentialsComponent {}
+export class CredentialsComponent implements OnInit {
+	tokenId = '';
+
+	link: Link | undefined;
+
+	constructor(private route: ActivatedRoute) {}
+
+	ngOnInit() {
+		this.tokenId = this.route.snapshot.paramMap.get('token') || '';
+		this.getLinkData();
+	}
+
+	getLinkData() {
+		// TODO: implement me
+		this.link = { email: 'testCal@gmail.com' };
+	}
+}
