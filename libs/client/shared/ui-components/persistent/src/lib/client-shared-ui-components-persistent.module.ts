d0b7fb9d29234c81575e3ea0d9a2b292f6a4b228
Afrah Ali <aali179@uottawa.ca>
2022-03-10 15:29:30 -0500

    feat(frontend): sidebar style

--- libs/client/shared/ui-components/persistent/src/lib/client-shared-ui-components-persistent.module.ts
+++ libs/client/shared/ui-components/persistent/src/lib/client-shared-ui-components-persistent.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatStepperModule } from '@angular/material/stepper/stepper-module';
+import { MatStepperModule } from '@angular/material/stepper';
 import { FooterComponent } from './footer/footer.component';
 import { StepperComponent } from './stepper/stepper.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @NgModule({
-	imports: [CommonModule, MatIconModule, MatButtonModule, MatStepperModule],
-	declarations: [FooterComponent, StepperComponent],
-	exports: [FooterComponent, StepperComponent],
+	imports: [CommonModule, MatIconModule, MatButtonModule, MatStepperModule, MatSidenavModule, MatButtonToggleModule],
+	declarations: [FooterComponent, StepperComponent, SidebarComponent],
+	exports: [FooterComponent, StepperComponent, SidebarComponent],
 })
 export class ClientSharedUiComponentsPersistentModule {}
