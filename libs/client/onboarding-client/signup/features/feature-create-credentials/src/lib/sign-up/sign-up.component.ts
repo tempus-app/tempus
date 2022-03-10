e8d7eb83d38247793760c3c227972a2ff4e2b56c
Mustafa Ali <alimustafa0987@gmail.com>
2022-03-09 19:54:04 -0500

    chore: undo password validator removal

--- libs/client/onboarding-client/signup/features/feature-create-credentials/src/lib/sign-up/sign-up.component.ts
+++ libs/client/onboarding-client/signup/features/feature-create-credentials/src/lib/sign-up/sign-up.component.ts
@@ -24,6 +24,7 @@ export class SignUpComponent implements OnInit {
 				email: new FormControl({ value: this.email, disabled: true }, Validators.required),
 				password: new FormControl(null, [
 					Validators.required,
+					// Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/),
 					// taken from: https://stackoverflow.com/questions/40529817/reactive-forms-mark-fields-as-touched
 				]),
 				confirmPassword: new FormControl(null, Validators.required),
