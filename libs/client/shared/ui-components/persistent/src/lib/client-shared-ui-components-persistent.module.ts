a2aaff4a6813184d75d0375a92a033045fdfa8a5
Afrah Ali <aali179@uottawa.ca>
2022-03-17 18:24:40 -0400

    fix(frontend): remove import

--- libs/client/shared/ui-components/persistent/src/lib/client-shared-ui-components-persistent.module.ts
+++ libs/client/shared/ui-components/persistent/src/lib/client-shared-ui-components-persistent.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatStepperModule } from '@angular/material/stepper';
-import { OnboardingClientSignupFeatureUploadResumeModule } from '@tempus/onboarding-client/signup/feature-upload-resume';
 import { FooterComponent } from './footer/footer.component';
 import { StepperComponent } from './stepper/stepper.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -11,15 +10,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @NgModule({
-	imports: [
-		CommonModule,
-		MatIconModule,
-		MatButtonModule,
-		MatStepperModule,
-		MatSidenavModule,
-		MatButtonToggleModule,
-		OnboardingClientSignupFeatureUploadResumeModule,
-	],
+	imports: [CommonModule, MatIconModule, MatButtonModule, MatStepperModule, MatSidenavModule, MatButtonToggleModule],
 	declarations: [FooterComponent, StepperComponent, SidebarComponent],
 	exports: [FooterComponent, StepperComponent, SidebarComponent],
 })
