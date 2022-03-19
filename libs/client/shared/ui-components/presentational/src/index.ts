0ecc4c2207e08aa657d5c95344dc65a2c15dc6f7
Afrah Ali <aali179@uottawa.ca>
2022-03-19 02:30:13 -0400

    feat(frontend): add sidebar logout, enums

--- libs/client/shared/ui-components/presentational/src/index.ts
+++ libs/client/shared/ui-components/presentational/src/index.ts
@@ -2,3 +2,5 @@ export * from './lib/client-shared-ui-components-presentational.module';
 export * from './lib/table/column.model';
 export * from './lib/table/function.model';
 export * from './lib/table/table-data.model';
+export * from './lib/button/button-type-enum';
+export * from './lib/button/button.component';
