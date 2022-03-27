7991d511bfdc8a9765e5aacfd8f9eac5eb06b0be
Ruwani <ruwanide51@gmail.com>
2022-03-27 19:22:16 -0400

    chore: test commit

--- libs/client/onboarding-client/signup/features/feature-upload-resume/src/lib/confirm-resume-upload/confirm-resume-upload.component.ts
+++ libs/client/onboarding-client/signup/features/feature-upload-resume/src/lib/confirm-resume-upload/confirm-resume-upload.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
 	selector: 'tempus-confirm-resume-upload',
