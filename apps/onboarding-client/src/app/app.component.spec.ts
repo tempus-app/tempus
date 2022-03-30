8625d3c35f222da18a9a7de449ef1ca39ec613dd
Mustafa Ali <alimustafa0987@gmail.com>
2022-03-29 23:05:54 -0400

    chore(client): fix test

--- apps/onboarding-client/src/app/app.component.spec.ts
+++ apps/onboarding-client/src/app/app.component.spec.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 describe('AppComponent', () => {
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			declarations: [AppComponent],
-			imports: [HttpClientModule],
+			imports: [HttpClientModule, TranslateModule.forRoot()],
 		}).compileComponents();
 	}));
 
