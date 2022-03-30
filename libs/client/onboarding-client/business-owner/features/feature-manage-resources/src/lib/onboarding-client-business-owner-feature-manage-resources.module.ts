5295123bfba1d9537f4dd7bf1928357c092af8e9
Aman <amanriat777@gmail.com>
2022-03-30 00:57:12 -0400

    feat(frontend): revert changes to manage resources

--- libs/client/onboarding-client/business-owner/features/feature-manage-resources/src/lib/onboarding-client-business-owner-feature-manage-resources.module.ts
+++ libs/client/onboarding-client/business-owner/features/feature-manage-resources/src/lib/onboarding-client-business-owner-feature-manage-resources.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { ClientSharedUiComponentsPersistentModule } from '@tempus/client/shared/ui-components/persistent';
 import { ManageResourcesComponent } from './manage-resources/manage-resources.component';
 
 @NgModule({
 	imports: [
 		CommonModule,
-		ClientSharedUiComponentsPersistentModule,
 		RouterModule.forChild([
 			{
 				path: '',
