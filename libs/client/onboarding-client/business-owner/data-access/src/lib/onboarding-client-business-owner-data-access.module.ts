866a87e7fd6bbcd7fd28c39e697938a4af614685
Mustafa Ali <alimustafa0987@gmail.com>
2022-03-29 23:30:48 -0400

    chore(client): register business owner store

--- libs/client/onboarding-client/business-owner/data-access/src/lib/onboarding-client-business-owner-data-access.module.ts
+++ libs/client/onboarding-client/business-owner/data-access/src/lib/onboarding-client-business-owner-data-access.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { BUSINESS_OWNER_FEATURE_KEY, reducers } from './+state';
+import { EffectsModule } from '@ngrx/effects';
+import { TestEffects } from './+state/test/test.effects';
 
 @NgModule({
-	imports: [CommonModule],
+	imports: [
+		CommonModule, 
+		StoreModule.forFeature(BUSINESS_OWNER_FEATURE_KEY, reducers),
+		EffectsModule.forFeature([TestEffects]),
+	],
 })
 export class OnboardingClientBusinessOwnerDataAccessModule {}
