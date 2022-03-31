4e3a2e270507de2d17e9b1e32bfbb42ebed60daa
Afrah Ali <aali179@uottawa.ca>
2022-03-31 01:10:13 -0400

    feat(frontend): edit template

---
+++ libs/client/onboarding-client/resource/features/feature-profile/src/lib/edit-profile/edit-profile.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { OnboardingClientState, logout } from '@tempus/client/onboarding-client/shared/data-access';
+import { Subject } from 'rxjs';
+import { ButtonType } from '@tempus/client/shared/ui-components/presentational';
+import { UserType } from '@tempus/client/shared/ui-components/persistent';
+
+@Component({
+	selector: 'tempus-edit-profile',
+	templateUrl: './edit-profile.component.html',
+	styleUrls: ['./edit-profile.component.scss'],
+})
+export class EditProfileComponent implements OnDestroy {
+	constructor(private store: Store<OnboardingClientState>, private router: Router, private route: ActivatedRoute) {}
+
+	destroyed$ = new Subject<void>();
+
+	ButtonType = ButtonType;
+    UserType = UserType;
+
+    selectedTab(tab: string) {
+        if (tab == 'logout') {
+            console.log('click');
+            this.logout();
+        }
+    }
+
+	logout() {
+		this.store.dispatch(logout());
+		this.router.navigateByUrl('signin');
+	}
+
+	ngOnDestroy(): void {
+		this.destroyed$.next();
+		this.destroyed$.complete();
+	}
+}
--- libs/client/onboarding-client/resource/features/feature-profile/src/lib/onboarding-client-resource-feature-profile.module.ts
+++ libs/client/onboarding-client/resource/features/feature-profile/src/lib/onboarding-client-resource-feature-profile.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { ProfileComponent } from './profile/profile.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { ClientSharedUiComponentsPersistentModule } from '@tempus/client/shared/ui-components/persistent';
 import { ClientSharedUiComponentsPresentationalModule } from '@tempus/client/shared/ui-components/presentational';
+import { ClientSharedUiComponentsInputModule } from '@tempus/client/shared/ui-components/input';
 import { ClientSharedInputResourceInformationModule } from '@tempus/client/onboarding-client/shared/ui-components/input/resource-information';
 import { ClientSharedPresentationalResourceDisplayModule } from '@tempus/client/onboarding-client/shared/ui-components/presentational/resource-display';
 
@@ -14,7 +18,10 @@ import { ClientSharedPresentationalResourceDisplayModule } from '@tempus/client/
 		CommonModule,
 		MatIconModule,
 		MatTooltipModule,
+		FlexLayoutModule,
+		MatFormFieldModule,
 		ClientSharedUiComponentsPersistentModule,
+		ClientSharedUiComponentsInputModule,
 		ClientSharedUiComponentsPresentationalModule,
 		ClientSharedInputResourceInformationModule,
 		ClientSharedPresentationalResourceDisplayModule,
@@ -26,6 +33,6 @@ import { ClientSharedPresentationalResourceDisplayModule } from '@tempus/client/
 			},
 		]),
 	],
-	declarations: [ProfileComponent],
+	declarations: [ProfileComponent, EditProfileComponent],
 })
 export class OnboardingClientResourceFeatureProfileModule {}
