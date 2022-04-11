5064886cf2ca42670f39fe52a88cc2a48b6bbd2e
Ruwani <ruwanide51@gmail.com>
2022-04-11 13:59:47 -0400

    add seed and clear script

---
+++ utils/database/seed.ts
@@ -0,0 +1,36 @@
+import { NestFactory } from '@nestjs/core';
+import commandLineArgs from 'command-line-args';
+import { CommandLineArgsOptions, SeederService, SeedModule } from '../../libs/api/shared/feature-seed/src';
+
+const seederArgsInterface: commandLineArgs.OptionDefinition[] = [
+	{
+		name: 'clients',
+		type: Number,
+		defaultValue: 3,
+	},
+	{
+		name: 'projects',
+		type: Number,
+		defaultValue: 6, // 2 per client
+	},
+	{
+		name: 'resources',
+		type: Number,
+		defaultValue: 20,
+	},
+];
+async function bootstrap() {
+	const appContext = await NestFactory.createApplicationContext(SeedModule);
+
+	try {
+		const seeder = appContext.get(SeederService);
+		const options = commandLineArgs(seederArgsInterface);
+
+		await seeder.seed(options as CommandLineArgsOptions);
+	} catch (e) {
+		console.error(e);
+	} finally {
+		appContext.close();
+	}
+}
+bootstrap();
