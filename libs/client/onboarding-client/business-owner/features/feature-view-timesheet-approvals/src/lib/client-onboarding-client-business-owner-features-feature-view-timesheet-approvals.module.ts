410d21e3247b9c964bc85148288e77df8c8a25f2
alexchoukeir <alexander.choukeir@gmail.com>
2023-03-18 18:04:35 -0400

    Set up routing for the view timesheet approvals page

--- libs/client/onboarding-client/business-owner/features/feature-view-timesheet-approvals/src/lib/client-onboarding-client-business-owner-features-feature-view-timesheet-approvals.module.ts
+++ libs/client/onboarding-client/business-owner/features/feature-view-timesheet-approvals/src/lib/client-onboarding-client-business-owner-features-feature-view-timesheet-approvals.module.ts
@@ -1,9 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ClientSharedUiComponentsPresentationalModule } from '@tempus/client/shared/ui-components/presentational';
+import { HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { ViewTimesheetApprovalsComponent } from './view-timesheet-approvals/view-timesheet-approvals.component';
 
+function createTranslateLoader(http: HttpClient) {
+	return new TranslateHttpLoader(http, './assets/i18n/onboarding/owner/view-pending-approvals/', '.json');
+}
+
 @NgModule({
-	imports: [CommonModule],
+	imports: [
+		CommonModule,
+		ClientSharedUiComponentsPresentationalModule,
+		RouterModule.forChild([
+			{
+				path: '',
+				pathMatch: 'full',
+				component: ViewTimesheetApprovalsComponent,
+			},
+		]),
+		TranslateModule.forChild({
+			loader: {
+				provide: TranslateLoader,
+				useFactory: createTranslateLoader,
+				deps: [HttpClient],
+			},
+			isolate: false,
+			extend: true,
+		}),
+	],
 	declarations: [ViewTimesheetApprovalsComponent],
 })
 export class ClientOnboardingClientBusinessOwnerFeaturesFeatureViewTimesheetApprovalsModule {}
