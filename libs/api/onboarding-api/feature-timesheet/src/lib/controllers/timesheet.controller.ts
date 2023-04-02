9f1d9d33286ece6ed63eb5eacca9f4355c35aa9e
Zay <zghazal2001@gmail.com>
2023-04-02 14:19:15 -0400

    timesheet controller and services

--- libs/api/onboarding-api/feature-timesheet/src/lib/controllers/timesheet.controller.ts
+++ libs/api/onboarding-api/feature-timesheet/src/lib/controllers/timesheet.controller.ts
@@ -1,9 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '@tempus/api/shared/feature-auth';
+import { Timesheet } from '@tempus/shared-domain';
+import { CreateTimesheetDto, UpdateTimesheetDto } from '@tempus/api/shared/dto';
 import { TimesheetService } from '../services/timesheet.service';
 
 @ApiTags('Timesheet')
 @Controller('timesheet')
 export class TimesheetController {
 	constructor(private timesheetService: TimesheetService) {}
+
+	@UseGuards(JwtAuthGuard)
+	@Get('/:timesheetId')
+	async getTimesheet(@Param('timesheetId') timesheetId: number) {
+		return this.timesheetService.getTimesheet(timesheetId);
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Get('')
+	async getTimesheetsForUser(@Param('userId') userId: number): Promise<Timesheet[]> {
+		return this.timesheetService.getAllTimesheetsforUser(userId);
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Post('/')
+	async createTimesheet(@Body() timesheet: CreateTimesheetDto): Promise<Timesheet> {
+		return this.timesheetService.createTimesheet(timesheet);
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Patch('/:timesheetId')
+	async editTimesheet(@Body() updateTimesheetDto: UpdateTimesheetDto): Promise<Timesheet> {
+		return this.timesheetService.updateTimesheet(updateTimesheetDto);
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Delete('/:timesheetId')
+	async deleteTimesheet(@Param('timesheetId') timesheetId: number) {
+		return this.timesheetService.deleteTimesheet(timesheetId);
+	}
 }
--- libs/api/onboarding-api/feature-timesheet/src/lib/services/timesheet.service.ts
+++ libs/api/onboarding-api/feature-timesheet/src/lib/services/timesheet.service.ts
@@ -1,7 +1,49 @@
-import { Injectable } from '@nestjs/common';
-import { CommonService } from '@tempus/api/shared/feature-common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TimesheetEntity } from '@tempus/api/shared/entity';
+import { Repository } from 'typeorm';
+import { Timesheet } from '@tempus/shared-domain';
+import { CreateTimesheetDto } from '@tempus/api/shared/dto';
+// import {UpdateTimesheetDto } from '@tempus/api/shared/dto';
 
 @Injectable()
 export class TimesheetService {
-	constructor(private commonService: CommonService) {}
+	constructor(
+		@InjectRepository(TimesheetEntity)
+		private timesheetRepository: Repository<TimesheetEntity>,
+	) {}
+
+	async getTimesheet(timesheetId: number): Promise<Timesheet> {
+		const timesheetEntity = await this.timesheetRepository.findOne(timesheetId);
+		if (!timesheetEntity) throw new NotFoundException(`Could not find timesheet with id ${timesheetId}`);
+		return timesheetEntity;
+	}
+
+	async getAllTimesheetsforUser(userId: number): Promise<Timesheet[]> {
+		const timesheets = await this.timesheetRepository.find(userId);
+
+		return timesheets;
+	}
+
+	async createTimesheet(timesheet: CreateTimesheetDto): Promise<Timesheet> {
+		const timesheetEntity = TimesheetEntity.fromDto(timesheet);
+		return this.timesheetRepository.save(timesheetEntity);
+	}
+
+	/* async updateTimesheet(updateTimesheetDto: UpdateTimesheetDto): Promise<Timesheet> {
+			const timesheetEntity = await this.getTimesheet(updateTimesheetDto.id);
+	
+			for (const [key, val] of Object.entries(updateTimesheetDto)){
+				if (!val) {
+					delete updateTimesheetDto[key];
+				}
+			}
+			Object.assign(timesheetEntity, updateTimesheetDto);
+			return this.timesheetRepository.save(timesheetEntity);
+		} */
+
+	async deleteTimesheet(timesheetId: number) {
+		const timesheetEntity = await this.getTimesheet(timesheetId);
+		await this.timesheetRepository.remove(timesheetEntity);
+	}
 }
