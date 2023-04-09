a8b555eb8eecd16b81df0b58166fb25430aca928
Zay <zghazal2001@gmail.com>
2023-04-09 00:29:04 -0400

    fixes

--- libs/client/onboarding-client/resource/data-access/src/lib/onboarding-client-resource-data-access.module.ts
+++ libs/client/onboarding-client/resource/data-access/src/lib/onboarding-client-resource-data-access.module.ts
@@ -4,12 +4,13 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { reducers, RESOURCE_FEATURE_KEY } from './+state';
 import { ResourceEffects } from './+state/resource/resource.effects';
+import { TimesheetEffects } from './+state/view-resource-timesheets/view-resource-timesheets.effects';
 
 @NgModule({
 	imports: [
 		CommonModule,
 		StoreModule.forFeature(RESOURCE_FEATURE_KEY, reducers),
-		EffectsModule.forFeature([ResourceEffects]),
+		EffectsModule.forFeature([ResourceEffects, TimesheetEffects]),
 	],
 })
 export class OnboardingClientResourceDataAccessModule {}
