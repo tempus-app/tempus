aaab8f96ac85f34e235de72d4f7b85f49145456b
Ilyas Hassan <ilyashassan4400@gmail.com>
2023-09-28 23:01:21 -0400

    Added models and dto's

--- libs/shared/domain/src/lib/dtos/timesheet-dtos/timesheet/IcreateTimesheet.dto.ts
+++ libs/shared/domain/src/lib/dtos/timesheet-dtos/timesheet/IcreateTimesheet.dto.ts
@@ -1,17 +1,1 @@
-import { ICreateTimesheetEntryDto } from "../timesheet-entry";
-
-export interface ICreateTimesheetDto {
-
-	projectId?: number;
-	resourceId?:number;
-	supervisorId?: number;
-	weekStartDate?: Date;
-	weekEndDate?: Date;
-	approvedBySupervisor?: boolean;
-	approvedByClient?: boolean;
-	supervisorComment?: string;
-	clientRepresentativeComment?: string;
-	audited?: boolean;
-	billed?: boolean;
-	timesheetEntries?: ICreateTimesheetEntryDto[];
-}
+export * from './timesheet-models';
---
+++ libs/shared/domain/src/lib/models/report-models/report.model.ts
@@ -0,0 +1,10 @@
+export interface Report {
+	id: number;
+	name: string;
+	title: string;
+	clientName: string;
+	workType: string;
+	hoursWorked: number;
+	costRate: number;
+	billingRate: number;
+}
