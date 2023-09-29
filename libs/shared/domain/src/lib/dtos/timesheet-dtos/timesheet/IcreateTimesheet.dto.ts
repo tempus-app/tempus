516b39f1ef0ad799bafb2f411644efd43e6422de
Ilyas Hassan <ilyashassan4400@gmail.com>
2023-09-29 12:05:21 -0400

    Fixed issue with removed dto

--- libs/shared/domain/src/lib/dtos/timesheet-dtos/timesheet/IcreateTimesheet.dto.ts
+++ libs/shared/domain/src/lib/dtos/timesheet-dtos/timesheet/IcreateTimesheet.dto.ts
@@ -1,1 +1,17 @@
-export * from './timesheet-models';
+import { ICreateTimesheetEntryDto } from "../timesheet-entry";
+
+export interface ICreateTimesheetDto {
+
+	projectId?: number;
+	resourceId?:number;
+	supervisorId?: number;
+	weekStartDate?: Date;
+	weekEndDate?: Date;
+	approvedBySupervisor?: boolean;
+	approvedByClient?: boolean;
+	supervisorComment?: string;
+	clientRepresentativeComment?: string;
+	audited?: boolean;
+	billed?: boolean;
+	timesheetEntries?: ICreateTimesheetEntryDto[];
+}
