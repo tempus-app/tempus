d155387b266a760cef9736e7a0a5a34f490f8f77
Alex <55166154+alexchoukeir@users.noreply.github.com>
2023-10-04 23:56:01 -0400

    Created timesheet table component

---
+++ libs/client/onboarding-client/shared/ui-components/input/resource-information/src/lib/timesheet/timesheet.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'tempus-timesheet-table',
+	templateUrl: './timesheet.component.html',
+	styleUrls: ['./timesheet.component.css'],
+})
+export class TimesheetComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit(): void {}
+}
