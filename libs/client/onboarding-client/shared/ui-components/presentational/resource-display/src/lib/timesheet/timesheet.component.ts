379186f5efd8e9b04a602a82f6104ca477b61aa5
Alex <55166154+alexchoukeir@users.noreply.github.com>
2023-10-07 21:50:03 -0400

    Created timesheet resource display

---
+++ libs/client/onboarding-client/shared/ui-components/presentational/resource-display/src/lib/timesheet/timesheet.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'tempus-resource-display-timesheet',
+	templateUrl: './timesheet.component.html',
+	styleUrls: ['./timesheet.component.scss'],
+})
+export class TimesheetComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit(): void {}
+}
