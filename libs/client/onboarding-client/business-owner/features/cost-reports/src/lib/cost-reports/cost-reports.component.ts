1830e793439add1e1374e604daab77c220582334
Ovais09 <ovazeem100@gmail.com>
2023-12-02 02:46:33 -0500

    populated the dropdown menus for supervisor

--- libs/client/onboarding-client/business-owner/features/cost-reports/src/lib/cost-reports/cost-reports.component.ts
+++ libs/client/onboarding-client/business-owner/features/cost-reports/src/lib/cost-reports/cost-reports.component.ts
@@ -1,6 +1,13 @@
 // cost-reports.component.ts
 import { Component, OnInit } from '@angular/core';
 import { Column, TableDataModel } from '@tempus/client/shared/ui-components/presentational';
+import {
+	OnboardingClientState,
+	selectLoggedInUserId,
+	OnboardingClientResourceService,
+} from '@tempus/client/onboarding-client/shared/data-access';
+import { Store } from '@ngrx/store';
+import { HttpClient } from '@angular/common/http';
 import { ReportService } from './report.service';
 
 export interface CostReport {
@@ -27,8 +34,12 @@ export interface CostReportsTableData extends TableDataModel, CostReport {
 })
 export class CostReportsComponent implements OnInit {
 	costReportsTableData: CostReportsTableData[] = [];
+
 	pageNum = 1;
+
 	totalCostReports = 1;
+
+	supervisorId: number = 0;
 
 	dropdownOptions: { [key: string]: any[] } = {};
 
@@ -85,16 +96,67 @@ export class CostReportsComponent implements OnInit {
 	// 	},
 	// ];
 
-	constructor(private reportService: ReportService) {}
+	constructor(
+		private reportService: ReportService,
+		private store: Store<OnboardingClientState>,
+		private resourceService: OnboardingClientResourceService,
+		private http: HttpClient,
+	) {}
+
 	ngOnInit(): void {
-		// this.loadReports();
-		this.initializeDropdownOptions();
+		this.store.select(selectLoggedInUserId).subscribe(id => {
+			if (id) {
+				this.supervisorId = id;
+				console.log('Supervisor ID:', id);
+				this.resourceService.getSupervisorProjects(id).subscribe(projects => {
+					console.log('Projects for Supervisor:', projects);
+					// Process the projects data here if needed
+					this.dropdownOptions['project'] = projects.map(p => p.name);
+
+					// Extract unique months and years from project start dates
+					const months = new Set();
+					const years = new Set();
+					projects.forEach(project => {
+						const startDate = new Date(project.startDate);
+						const month = startDate.toLocaleString('default', { month: 'long' });
+						const year = startDate.getFullYear();
+
+						months.add(month);
+						years.add(year);
+					});
+
+					// Update the dropdown options for months and years
+					this.dropdownOptions['month'] = Array.from(months);
+					this.dropdownOptions['year'] = Array.from(years);
+				});
+				this.resourceService.getSupervisorClients(id).subscribe(clients => {
+					console.log('Clients for Supervisor:', clients);
+					// Process the projects data here if needed
+					this.dropdownOptions['client'] = clients.map(c => c.clientName);
+					// Process the projects data here if needed
+				});
+				this.resourceService.getSupervisorResources(id).subscribe(resources => {
+					console.log('Resources for Supervisor:', resources);
+					// Process the projects data here if needed
+					this.dropdownOptions['user'] = resources.map(r => r.firstName + ' ' + r.lastName);
+					// Process the projects data here if needed
+				});
+				// this.fetchProjectsForSupervisor();
+			}
+		});
 	}
 
+	// 	fetchProjectsForSupervisor(): void {
+	// 		this.reportService.getSupervisorProjects(this.supervisorId).subscribe(projects => {
+	// 				console.log('Projects for Supervisor:', projects);
+	// 				// Process the projects data here if needed
+	// 		});
+	// }
+
 	generateDropdowns(): void {
-    console.log('Generate Dropdowns clicked');
-    // Implement the logic as needed
-  }
+		console.log('Generate Dropdowns clicked');
+		// Implement the logic as needed
+	}
 
 	initializeDropdownOptions(): void {
 		const dropdownNames = ['project', 'user', 'client', 'month', 'year'];
